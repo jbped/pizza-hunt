ee4f87da63deb6e85aae2537db10f452b3b983ec
Jake P <76881086+jbped@users.noreply.github.com>
2021-08-17 18:33:12 -0600

    added reply functionality

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -5,8 +5,11 @@ const commentController = {
         Comment.create(body)
             .then(({ _id }) => {
                 return Pizza.findOneAndUpdate(
+                    // query param
                     { _id: params.pizzaId },
+                    // Pushes comments with the appropriate ID to the appropriate pizza that they should be associated with
                     { $push: { comments: _id } },
+                    // Returns the updated pizza
                     { new: true }
                 )
             })
@@ -27,8 +30,11 @@ const commentController = {
                     return;
                 }
                 return Pizza.findOneAndUpdate(
+                    // Finds the pizza the comment is associated with
                     { _id: params.pizzaId },
+                    // Pull removes the comment from the Pizza Comments array
                     { $pull: { comments: params.commentId } },
+                    // Returns the updated pizza
                     { new: true }
                 );
             })
@@ -39,6 +45,36 @@ const commentController = {
                 }
                 res.json(dbPizzaData);
             })
+            .catch(err => res.json(err));
+    },
+    addReply({ params, body }, res) {
+        Comment.findOneAndUpdate(
+            // Finds the comment that the new reply will be associated with
+            { _id: params.commentId },
+            // Pushes the new reply to the replies arr
+            { $push: { replies: body } },
+            // Returns the updated comment
+            { new: true }
+        )
+            .then(dbPizzaData => {
+                if (!dbPizzaData) {
+                    res.status(404).json({ message: 'No pizza found with this id!' });
+                    return;
+                }
+                res.json(dbPizzaData);
+            })
+            .catch(err => res.json(err));
+    },
+    removeReply({ params }, res) {
+        Comment.findOneAndDelete(
+            // Finds the comment that the new reply will be associated with
+            { _id: params.commentId },
+            // Pulls the deleted reply from the replies arr
+            { $pull: { replyId: params.replyId } },
+            // Returns the updated comment
+            { new: true }
+        )
+            .then(dbPizzaData => res.json(dbPizzaData))
             .catch(err => res.json(err));
     }
 }
--- models/Comment.js
+++ models/Comment.js
@@ -1,13 +1,50 @@
-const { Schema, model } = require("mongoose");
+const { Schema, model, Types } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
 
-const CommentSchema = new Schema ({
-    writtenBy: String,
-    commentBody: String,
-    createdAt: {
-        type: Date,
-        default: Date.now
+const ReplySchema = new Schema(
+    {
+        replyId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        replyBody: String,
+        writtenBy: String,
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        },
     }
-});
+);
+
+const CommentSchema = new Schema(
+    {
+        writtenBy: String,
+        commentBody: String,
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        },
+        replies: [ReplySchema]
+    },
+    {
+        toJSON: {
+            getters: true,
+            virtuals: true
+        },
+        id: false
+    }
+);
+
+CommentSchema.virtual("replyCount").get(function() {
+    return this.replies.length
+})
 
 const Comment = new model("Comment", CommentSchema);
 
